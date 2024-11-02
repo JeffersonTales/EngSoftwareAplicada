593d7f773595d319acae7df2bc92180a38a4a47d
Jefferson Tales Canalli <jefferson.canalli@hotmail.com>
2024-11-02 19:41:08 -0300

    commit 1

---
+++ EngSoftwareAplicada/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        // Configuração do Builder para carregar o appsettings.json
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+
+        // Leitura de valores de configuração
+        string appName = config["ApplicationName"];
+        string defaultConnection = config.GetSection("ConnectionStrings")["DefaultConnection"];
+
+        Console.WriteLine($"Application Name: {appName}");
+        Console.WriteLine($"Default Connection: {defaultConnection}");
+    }
+}
